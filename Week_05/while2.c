93b28c384053ee40167f3f26abacde9023b3fcf4
Marcel Jar <marcel.jar@senecacollege.ca>
2023-09-06 09:04:43 -0400

    Create while2.c

---
+++ Week_05/while2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int i = 0;
+
+    while (i < 10) {
+        printf("All work and no play makes Jack a dull boy!\n");
+        i = i + 1;
+    }
+
+    return 0;
+}
