2d63b8219e0377f53bac3eeffec6c92929ed83b3
Marcel Jar <marcel.jar@senecacollege.ca>
2023-09-13 09:34:33 -0400

    Update logical.c

--- Week_02/logical.c
+++ Week_02/logical.c
@@ -3,7 +3,7 @@
 int main() {
     int i = 0, j = 1;
 
-    printf(" %i AND %i => %i\n", i, i, i && i);
+    printf(" %i AND %i => %i\n", i, j, i && i);
     printf(" %i OR %i => %i\n", i, j, i || i);
     printf(" !%i => %i\n", i, !i);
     
