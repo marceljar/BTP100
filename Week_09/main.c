b5eaa14c5c2fadd71c14c47d3e37a9e4fa489825
Marcel Jar <marcel.jar@senecacollege.ca>
2023-11-10 13:29:57 -0500

    Create main.c

---
+++ Week_09/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#include "math.h"
+
+int main() {
+
+    printf("4 + 3 = %i\n", sum(4,3));
+    printf("4 - 3 = %i\n", minus(4,3));
+    printf("4 * 3 = %i\n", multi(4,3));
+    
+    return 0;
+}
